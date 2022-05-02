9085a75c225068bd252cde71caacf52f67ab6590
Lê Nhật Minh <lenhatminh11a1@gmail.com>
2022-05-03 00:07:31 +0700

    adding middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -5,12 +5,13 @@ import {
   getUserProfile,
   registerUser,
 } from "../controllers/userController.js";
+import { isAdminOrManager } from "../middlewares/isAdminOrManager.js";
 
 const router = express.Router();
 // POST API /api/login
 router.post("/login", authUser);
 router.post("/register", registerUser);
-router.get("/profile", isAuth, getUserProfile);
+router.get("/profile", isAuth, isAdminOrManager, getUserProfile);
 //router.put;
 //router.delete
 
